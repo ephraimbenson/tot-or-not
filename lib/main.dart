5c671bb953cb45321475633a0faf1973d81c9334
Ephraim Benson <ephraimbenson@users.noreply.github.com>
2024-10-27 15:24:20 -0700

    Upgrades Dart, restores functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -7,16 +6,44 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/gestures.dart';
 import 'package:instant/instant.dart';
 import 'package:intl/intl.dart';
+import 'package:html/parser.dart';
+import 'package:html/dom.dart' as dom;
 
-// Tots api at https://willbeddow.com/api/bonapp/v1/tots/
-// Full menu api at https://willbeddow.com/api/bonapp/v1/menu/
+//TODO: collapsable menu
+//TODO: tot presence highlights meal?
+
 final Uri _bonappUrl = Uri.parse('https://carleton.cafebonappetit.com/');
 DateTime northfieldMN = curDateTimeByZone(zone: "CST");
+
+final URL_BURTON = Uri.https(
+    'legacy.cafebonappetit.com', '/print-menu/cafe/35/menu/510645/days/today/');
+final URL_LDC = Uri.https(
+    'legacy.cafebonappetit.com', 'print-menu/cafe/36/menu/514247/days/today');
+
+const MEAL_DIVIDER_CLASS = "meal-types my-day-menu-table eni-day-menu";
 
 extension StringExtension on String {
   String capitalize() {
     return "${this[0].toUpperCase()}${this.substring(1).toLowerCase()}";
   }
+}
+
+Map<String, List> buildMenuFromHtml(dom.Document htmlDoc) {
+  var meal_map = Map<String, List>();
+
+  var meals = htmlDoc.getElementsByClassName(MEAL_DIVIDER_CLASS);
+  for (var m in meals) {
+    String meal_name =
+        m.getElementsByClassName('spacer day')[0].text.capitalize();
+    var meal_items = m.getElementsByTagName('p');
+
+    var result = meal_items.map((item) {
+      return item.text.trim().split(RegExp(r'\t'))[0].capitalize();
+    }).toList();
+    meal_map[meal_name] = result;
+  }
+
+  return meal_map;
 }
 
 bool stringSuggestsTots(String menuItem) {
@@ -30,48 +57,29 @@ bool stringSuggestsTots(String menuItem) {
 }
 
 Future<TodaysMenu> fetchTodaysMenu() async {
-  var url = Uri.https('willbeddow.com', '/api/bonapp/v1/menu/');
-  final response = await http.get(url);
-  if (response.statusCode == 200) {
-    return TodaysMenu.fromJson(json.decode(response.body));
+  var menuDataBurton, menuDataLdc;
+  final responseBurton = await http.get(URL_BURTON);
+  if (responseBurton.statusCode == 200) {
+    menuDataBurton = buildMenuFromHtml(parse(responseBurton.body));
   } else {
     throw Exception('Failed to load menu');
   }
+
+  final responseLdc = await http.get(URL_LDC);
+  if (responseLdc.statusCode == 200) {
+    menuDataLdc = buildMenuFromHtml(parse(responseLdc.body));
+  } else {
+    throw Exception('Failed to load menu');
+  }
+  return TodaysMenu.fromData(menuDataBurton, menuDataLdc);
 }
 
 class TodaysMenu {
   final Map theMenu;
-  TodaysMenu({this.theMenu});
+  TodaysMenu({required this.theMenu});
 
-  factory TodaysMenu.fromJson(Map<String, dynamic> json) {
-    var menuObj = {};
-    json.forEach(
-      (locationName, location) {
-        menuObj[locationName] = {'meals': [], 'tots': []};
-        location.forEach(
-          (event, dishList) {
-            menuObj[locationName]['meals'].add(event);
-            var foundTots = false;
-            for (var dish in dishList) {
-              // dish = 'tater tots'; // for testing
-              if (stringSuggestsTots(dish.toString())) {
-                dish = dish.toString().capitalize();
-                menuObj[locationName]['tots'].add('$dish! 🔥');
-                foundTots = true;
-                break;
-              }
-            }
-            if (!foundTots) {
-              menuObj[locationName]['tots'].add('No tots. 😥');
-            }
-          },
-        );
-        if (menuObj[locationName]['meals'].isEmpty) {
-          menuObj[locationName]['meals'].add("Closed today");
-          menuObj[locationName]['tots'].add('');
-        }
-      },
-    );
+  factory TodaysMenu.fromData(Map menuBurton, Map menuLdc) {
+    var menuObj = {'Burton': menuBurton, 'LDC': menuLdc};
     return TodaysMenu(
       theMenu: menuObj,
     );
@@ -87,38 +95,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tot or Not',
-      home: MyHomePage(title: 'Tot or Not'),
+      title: '🥔 Tot or Not 🥔',
+      home: MyHomePage(title: '🥔 Tot or Not 🥔'),
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.amber,
+        ),
       ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  Future<TodaysMenu> futureMenu;
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late Future<TodaysMenu> futureMenu;
+
+  static const List<Tab> myTabs = <Tab>[
+    Tab(text: 'Burton'),
+    Tab(text: 'LDC'),
+  ];
+
+  late TabController _tabController;
 
   Widget footer() {
     return Center(
-        heightFactor: 2.5,
         child: Padding(
-            padding: EdgeInsets.only(left: 10, right: 10),
+            padding: EdgeInsets.only(top: 15, bottom: 15),
             child: RichText(
               textAlign: TextAlign.center,
               text: TextSpan(
                 children: [
                   new TextSpan(
-                    text:
-                        'Made by Ephraim Benson. Carleton College menu data available at ',
+                    text: 'Made by Ephraim Benson. Menu sourced from ',
                     style: new TextStyle(color: Colors.black),
                   ),
                   new TextSpan(
@@ -137,7 +155,14 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(vsync: this, length: myTabs.length);
     updateMenu();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   void updateMenu() {
@@ -148,7 +173,8 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tot or Not',
+        backgroundColor: Colors.amber,
+        title: Text('🥔 Tot or Not 🥔',
             style: GoogleFonts.lobster(
               fontSize: 30,
               fontWeight: FontWeight.w900,
@@ -159,62 +185,73 @@ class _MyHomePageState extends State<MyHomePage> {
             onPressed: updateMenu,
           )
         ],
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: myTabs,
+        ),
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: 20),
-            child: Text(
-              DateFormat.yMMMd().format(northfieldMN),
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+      body: TabBarView(
+        controller: _tabController,
+        children: myTabs.map((Tab tab) {
+          final String locationName = tab.text!;
+          return Center(
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: Text(
+                    DateFormat.yMMMd().format(northfieldMN),
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Expanded(
+                    child: FutureBuilder<TodaysMenu>(
+                  future: futureMenu,
+                  builder: (context, snapshot) {
+                    Widget availableChild = CircularProgressIndicator();
+                    if (snapshot.hasData) {
+                      availableChild =
+                          _buildMenuList(snapshot.data?.theMenu, locationName);
+                    } else if (snapshot.hasError) {
+                      availableChild = Text("${snapshot.error}");
+                    }
+                    return Center(
+                      child: availableChild,
+                    );
+                  },
+                )),
+                Container(
+                  child: footer(),
+                  color: Colors.amberAccent,
+                ),
+              ],
             ),
-          ),
-          Expanded(
-              child: FutureBuilder<TodaysMenu>(
-            future: futureMenu,
-            builder: (context, snapshot) {
-              Widget availableChild = CircularProgressIndicator();
-              if (snapshot.hasData) {
-                availableChild = _buildMenuList(snapshot.data.theMenu);
-              } else if (snapshot.hasError) {
-                availableChild = Text("${snapshot.error}");
-              }
-              return Center(
-                child: availableChild,
-              );
-            },
-          ))
-        ],
+          );
+        }).toList(),
       ),
-      bottomSheet: footer(),
     );
   }
 
-  Widget _buildMenuList(menu) {
+  Widget _buildMenuList(menu, locationName) {
+    final menuItems = menu[locationName];
     List<ListTile> tiles = [];
-    // Make tile for each dining hall
-    menu.forEach(
-      (location, timesAndTots) {
-        tiles.add(_hallTile(location, Icons.restaurant_menu_rounded));
-        final meals = timesAndTots['meals'];
-        final tots = timesAndTots['tots'];
-        // Make tiles for meals
-        for (int i = 0; i < meals.length; i++) {
-          // Check newly generated display string for a fire emoji. That means tots!
-          final totsPresent = tots[i].toString().contains('🔥');
-          tiles.add(_mealTile(meals[i], tots[i], totsPresent));
-        }
-      },
-    );
+    tiles.add(_hallTile(locationName, Icons.restaurant));
+    menuItems.forEach((String mealName, dishes) {
+      tiles.add(_mealTile(mealName));
+      dishes.forEach((String dish) {
+        tiles.add(_dishTitle(
+            dish, stringSuggestsTots(dish), Icons.restaurant_menu_rounded));
+      });
+    });
 
     return ListView(
       children: tiles,
     );
   }
 
-  ListTile _hallTile(String title, IconData icon) => ListTile(
+  ListTile _hallTile(String name, IconData icon) => ListTile(
         title: Text(
-          title.toUpperCase(),
+          name,
           style: TextStyle(fontWeight: FontWeight.w700, fontSize: 22),
         ),
         leading: Icon(
@@ -224,20 +261,31 @@ class _MyHomePageState extends State<MyHomePage> {
         // trailing: Icon(Icons.keyboard_arrow_down),
       );
 
-  ListTile _mealTile(String title, String subtitle, bool totsPresent) =>
-      ListTile(
+  ListTile _mealTile(String title) => ListTile(
         title: Text(
           title,
           style: TextStyle(fontSize: 20),
         ),
-        subtitle: subtitle.isEmpty
-            ? null
-            : Text(subtitle, style: TextStyle(fontSize: 18)),
+        leading: Icon(
+          Icons.restaurant_menu,
+          color: Colors.black,
+          // size: 30,
+        ),
+        contentPadding: EdgeInsets.only(left: 40.0),
+      );
+
+  ListTile _dishTitle(String title, bool totsPresent, IconData icon) =>
+      ListTile(
+        title: Text(
+          title,
+          style: TextStyle(
+              fontWeight: totsPresent ? FontWeight.w700 : FontWeight.normal,
+              fontSize: 18),
+        ),
+        contentPadding: EdgeInsets.only(left: 60.0),
         leading: Icon(
           totsPresent ? Icons.check_circle : Icons.cancel_outlined,
           color: totsPresent ? Colors.green : Colors.red,
-          size: 30,
         ),
-        contentPadding: EdgeInsets.only(left: 40.0),
       );
 }
